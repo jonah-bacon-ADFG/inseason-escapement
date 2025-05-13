3fe05a79021ee381722f61f9f827f838247c48c4
jonah-bacon-ADFG <jonah.bacon@alaska.gov>
2025-05-12 16:21:44 -0800

    Commit alternative method for publishing Shiny App to web browser

---
+++ shiny-app-code/app.R
@@ -0,0 +1,10 @@
+# deployApp(appDir = "code",
+#           appName = "inseason_escapement_monitoring_app",
+#           appTitle = "In-Season LCI Escapement Monitoring App",
+#           # launch.browser = FALSE,
+#           appVisibility = "public"
+# )
+
+source('code/helpers.R')
+source('code/ui.R')
+source('code/server.R')
